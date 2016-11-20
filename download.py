de52cd6c8bfaecddcf6d74cf9cf10e0ea306b518
tpatricksullivan <tpatricksullivan@gmail.com>
2016-11-20 17:16:46 -0600

    fixed space in play names
    Signed-off-by: tpatricksullivan <tpatricksullivan@gmail.com>

--- download.py
+++ download.py
@@ -1,12 +1,12 @@
 import os
 import urllib2
 import re
-import createPlayMap
+import getPlayMetadata
 
 
 def main(input_file):
     url_template = 'http://www.folgerdigitaltexts.org/{0}/text'
-    play_map = createPlayMap.main(input_file)
+    play_map = getPlayMetadata.main(input_file)
     for play in play_map['play_code']:
         url = url_template.format(play)
         print('downloading and stripping play {0}: {1}'.format(play, url))
@@ -18,7 +18,7 @@ def main(input_file):
 def clean_html(url, play):
     u = urllib2.urlopen(url)
     if not os.path.exists('data/'):
-        os.mkdir('data/')
+        raise IOError('./data folder not found')
     f = open('data/{0}.txt'.format(play), 'w')
     for line in u:
         line = re.sub('<br/>', '', line)
@@ -29,4 +29,4 @@ def clean_html(url, play):
 
 
 if __name__ == "__main__":
-    main("playmap.csv")
+    main("playMetadata.csv")
--- getPlayMetadata.py
+++ getPlayMetadata.py
@@ -5,8 +5,11 @@ import pandas as pd
 def main(input_file = 'playmap.csv'):
 
     playmap = pd.read_csv(input_file)
+    playmap['play_code'] = playmap['play_code'].apply(lambda x: x.lower())
     assert isinstance(playmap, pd.DataFrame)
+
     return playmap
+
 
 if __name__ == "__main__":
     main('playmap.csv')
