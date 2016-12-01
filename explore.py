a8d5fe612361065862c2eebccb25dc5be21bc777
tpatricksullivan <tpatricksullivan@gmail.com>
2016-11-30 18:19:46 -0600

    changes after classroom presentation
    Signed-off-by: tpatricksullivan <tpatricksullivan@gmail.com>

--- explore.py
+++ explore.py
@@ -1,12 +1,10 @@
 import pandas as pd
-import getTextMetadata
-import cleanTaggedData
-import pandasql
 import numpy as np
-import analyze
 from matplotlib import pyplot as plt
-from sklearn.decomposition import PCA as sklearnPCA
-from sklearn.preprocessing import StandardScaler
+from collections import namedtuple
+
+
+import numpy as np
 from collections import namedtuple
 
 
@@ -45,124 +43,17 @@ def PC_Transform(inp, n_components):
     return inp.dot(topneigv)
 
 
-def plot_PCA(Y, md_):
-    # Coerce Y to a data frame if it is a matrix
-    Y = pd.DataFrame(Y, index = md.index)
+def plot_PCA(Y, md_, title_, p, fig_):
 
-    with plt.style.context('seaborn-whitegrid'):
-        plt.figure(figsize=(6, 4))
-        for lab, col in zip( md_['First_Folio_category'].unique(),
-                            ('blue', 'red', 'green', 'orange')):
-            txts = md['First_Folio_category'] == lab
-            plt.scatter(Y.loc[txts,0], Y.loc[txts,1], label=lab, c=col)
-        for pl in Y.index:
-            plt.text(Y.loc[pl, 0], Y.loc[pl, 1], pl)
-        plt.xlabel('Principal Component 1')
-        plt.ylabel('Principal Component 2')
-        plt.legend(loc='lower left')
-        plt.tight_layout()
-        plt.show()
+    fig_.add_subplot(1,2,p)
+    for lab, col in zip( md_['First_Folio_category'].unique(),
+                        ('blue', 'red', 'green', 'orange')):
+        txts = md_['First_Folio_category'] == lab
+        plt.scatter(Y.loc[txts,0], Y.loc[txts,1], label=lab, c=col, alpha = 0.4, s = 70)
+    for pl in Y.index:
+        plt.text(Y.loc[pl, 0]+0.1, Y.loc[pl, 1]+0.1, pl, fontsize = 18)
+    plt.xlabel('Principal Component 1', fontsize = 14)
+    plt.ylabel('Principal Component 2', fontsize = 14)
+    plt.title(title_, fontsize = 20)
 
     return 0
-
-
-md, A_tg, A_md, A_ng = analyze.analyze()
-
-
-
-A_tg, A_md = cleanTaggedData.get_tagged_texts()
-md =  getTextMetadata.main()
-justPlays = list(md.query('First_Folio_category != "Other"').index)
-justShak = list(md.index[md['Shakespeare']])
-justPlays_tg = A_tg.loc[ justPlays ]
-justShak_tg = A_tg.loc[ justShak ]
-A_tg.iloc[:,0:5].head()
-A_ng = cleanTaggedData.get_ngrams()
-justPlays_ng = A_ng.loc[justPlays]
-justShak_ng = A_ng.loc[justShak]
-A_ng.iloc[:,0:5].head()
-
-A_ng_pc1 = PC_Transform(A_ng,2)
-
-sklearn_pca = sklearnPCA(n_components = 2, svd_solver = 'full')
-A_tg_pc = sklearn_pca.fit_transform(A_tg)
-#df_justS = df_tg.index.isin(['prideandprejudice','leviathan'])
-
-
-with plt.style.context('fivethirtyeight'):
-    plt.figure(figsize=(6, 4))
-    for lab, col in zip(('History', 'Comedy', 'Tragedy'),
-                        ('blue', 'red', 'green')):
-        pls = text_md.First_Folio_category[ text_md['First_Folio_category']==lab].index
-        df_tg[~df_justS]
-        plt.scatter(df_transform[pls, 0],
-                    df_transform[pls, 1],
-                    label=lab,
-                    c=col)
-    #for i in range(0,39):
-    #    plt.text( df_transform[i,0], df_transform[i,1], df_md.text_key[i])
-    #plt.text( df_transform[:,0], df_transform[:,1], df_tg.index)
-    plt.xlabel('Principal Component 1')
-    plt.ylabel('Principal Component 2')
-    plt.legend(loc='lower left')
-    plt.tight_layout()
-    plt.show()
-
-
-
-## Plot histogram of each variable
-
-## http://www.gutenberg.org/cache/epub/1041/pg1041.txt
-
-
-
-
-
-# with matplotlib.pyplot.style.context('fivethirtyeight'):
-#     matplotlib.pyplot.figure(figsize=(6, 4))
-#     for lab, col in zip(('History', 'Comedy', 'Tragedy'),
-#                         ('blue', 'red', 'green')):
-#         pls = df_md_cat.First_Folio_category[ df_md_cat['First_Folio_category']==lab].index
-#         matplotlib.pyplot.scatter(Y_sklearn[pls, 0],
-#                     Y_sklearn[pls, 1],
-#                     label=lab,
-#                     c=col)
-#     for i in range(0,39):
-#         matplotlib.pyplot.text( Y_sklearn[i,0], Y_sklearn[i,1], df_md_cat.text_key[i])
-#     matplotlib.pyplot.text( 1,1, 'hello' )
-#     matplotlib.pyplot.xlabel('Principal Component 1')
-#     matplotlib.pyplot.ylabel('Principal Component 2')
-#     matplotlib.pyplot.legend(loc='lower left')
-#     matplotlib.pyplot.tight_layout()
-#     matplotlib.pyplot.show()
-#
-# sklearn = sklearnPCA(n_components = 3)
-# Y_sklearn = sklearn.fit_transform(df)
-# # from mpl_toolkits.mplot3d import Axes3D
-#
-# with matplotlib.pyplot.style.context('fivethirtyeight'):
-#     fig = matplotlib.pyplot.figure(figsize=(6, 4))
-#     fig.add_
-#     for lab, col in zip(('History', 'Comedy', 'Tragedy'),
-#                         ('blue', 'red', 'green')):
-#         pls = df_md_cat.First_Folio_category[ df_md_cat['First_Folio_category']==lab].index
-#         Axes3D.scatter(Y_sklearn[pls, 0],
-#                     Y_sklearn[pls, 1],
-#                     Y_sklearn[pls, 2],
-#                     label=lab,
-#                     c=col)
-#     # for i in range(0,39):
-#     #     matplotlib.pyplot.text( Y_sklearn[i,0], Y_sklearn[i,1], df_md_cat.text_key[i])
-#     # matplotlib.pyplot.text( 1,1, 'hello' )
-#     # matplotlib.pyplot.xlabel('Principal Component 1')
-#     # matplotlib.pyplot.ylabel('Principal Component 2')
-#     # matplotlib.pyplot.legend(loc='lower left')
-#     # matplotlib.pyplot.tight_layout()
-#     # matplotlib.pyplot.show()
-#
-#
-#
-# matplotlib.pyplot.imshow (sklearn_pca.components_)
-
-
-
